55f095439a44ed293d005165ea45d7837367f633
fouad lmrini <fouad11.lamrini@gmail.com>
2024-11-06 18:23:19 +0100

    faire le quezz

---
+++ js/script.js
@@ -0,0 +1,106 @@
+let button1=document.getElementById("button1");
+button1.addEventListener("click",function(){
+    display_non()
+}
+
+);
+
+
+function display_non(){
+let quezz =document.getElementById("quezz");
+
+    quezz.style.display = "block";
+
+
+
+
+
+
+}
+
+
+
+////////////////////////////////////////
+const questions = [{
+    question: "1.comment tu t'appel?",
+    options: ["fouad", "ayoub", "amine","abdesatar"],
+    correct: 0
+},
+{
+    question: "2.le roi de la foret?",
+    options: ["le lion", "le tigre", "le chien","le chat"],
+    correct: 0
+},
+{
+    question: "3.auxiliaire?",
+    options: ["manger", "etre", "prendre","venir"],
+    correct: 1
+},
+{
+    question: "4.premiere personne de pluriel?",
+    options: ["je", "ils", "nous","tu"],
+    correct: 2
+},
+{
+    question: "5.nous dison à quelqu'un qui nous aide?",
+    options: [" pardon", "derien", "merci","pourquoi"],
+    correct: 2
+}
+
+
+]
+
+let currentQuestion = 0; //pour suivre les question 
+
+
+//mis a jour la contenu des options
+function displayQuestion() {
+    const questionElement = document.getElementById("question");
+    questionElement.innerText = questions[currentQuestion].question;
+    document.getElementById("option1").innerText = questions[currentQuestion].options[0];
+    document.getElementById("option2").innerText = questions[currentQuestion].options[1];
+    document.getElementById("option3").innerText = questions[currentQuestion].options[2];
+    document.getElementById("option4").innerText = questions[currentQuestion].options[3];
+
+}
+
+//fonction qui verifier la reponse
+
+function selectOption(selected){if (selected===questions[currentQuestion].correct)
+{alert("la reponse est correct");}
+else{alert("faux");}
+}
+
+//fonction qui pase a autre question
+
+function nextQuestion(){
+currentQuestion++;
+if(currentQuestion<questions.length){
+    displayQuestion();
+}
+else{alert("le quezz est fini");
+    currentQuestion=0;
+    displayQuestion();
+}
+
+}
+
+// liee chaque button par option
+
+document.getElementById("option1").addEventListener("click",function(){
+    selectOption(0);
+});
+document.getElementById("option2").addEventListener("click",function(){
+    selectOption(1);
+});
+document.getElementById("option3").addEventListener("click",function(){
+    selectOption(2);
+});
+document.getElementById("option4").addEventListener("click",function(){
+    selectOption(3);
+});
+//liee button suivant par fonction nextQuestion
+document.getElementById("next").addEventListener("click",nextQuestion);
+
+//afficher premier question
+displayQuestion();
